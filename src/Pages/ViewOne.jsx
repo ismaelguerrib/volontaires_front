e8e98092beeba7fb83330e2b17ebe47ea3fb0b96
Ismaël Guerrib <ismael.guerrib@hotmail.fr>
2019-07-18 15:06:02 +0200

    a

--- src/Pages/ViewOne.jsx
+++ src/Pages/ViewOne.jsx
@@ -4,7 +4,7 @@ import Header from "./../Component/Header";
 
 export default class Details extends Component {
   state = {
-    cards: []
+    offer: []
   };
   // componentDidMount = () => {
   //   Axios.get(
