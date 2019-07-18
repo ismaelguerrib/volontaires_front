a20162aae44f3ab477a1bc33e769c57848bc3aa4
Stefano Vanotti <vanottistefano@gmail.com>
2019-07-18 15:43:25 +0200

    axios

--- src/Pages/ViewAll.jsx
+++ src/Pages/ViewAll.jsx
@@ -5,21 +5,46 @@ export default class ViewAll extends Component {
   state = {
     cards: []
   };
-  if() {}
+
   componentDidMount = () => {
-    Axios.get(`${process.env.REACT_APP_BACK_URL}/api/offers`)
-      .then(dbres => {
-        this.setState({ cards: dbres.data });
-      })
-      .catch(err => {
-        console.log(err);
-      });
-  };
+    if (this.props.match.path === "/i-want-to-be-helped") {
+      console.log("yooo");
+
+      Axios.get(`${process.env.REACT_APP_BACK_URL}/api/offers`)
+        .then(dbres => {
+          console.log(dbres.data);
+          this.setState({ cards: dbres.data });
+          console.log(this.state.cards);
+
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+    else {
+      console.log("weeee uaglio");
+
+      Axios.get(`${process.env.REACT_APP_BACK_URL}/api/requests`)
+        .then(dbres => {
+          console.log(dbres);
+
+          this.setState({ cards: dbres.data });
+          console.log(this.state.cards);
+
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    }
+  }
+
 
   render() {
+
     return (
       <div>
-        <p>{this.state.cards}</p>
+
+        {/* <p>{this.state.cards}</p> */}
       </div>
     );
   }
