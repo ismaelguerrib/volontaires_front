f21bbedce560c51bc7a333d4acd1fb9789a2e992
Stefano Vanotti <vanottistefano@gmail.com>
2019-07-25 12:43:35 +0200

    accepttask

--- src/Pages/AcceptedTasks.jsx
+++ src/Pages/AcceptedTasks.jsx
@@ -5,17 +5,47 @@ const handler = new apiHandler(process.env.REACT_APP_BACK_URL);
 export default class AcceptedTasks extends Component {
   state = {
     user: {},
-    task: {}
+    task: []
   }
 
   componentDidMount() {
+    var tasks = [];
     this.setState({ user: this.props.user });
+
+
+
+    handler.get("api/offers/requestinguser/" + this.props.user.id).then(res => {
+      res.data.forEach(element => {
+        if (element.isAccepet) {
+          tasks.push(element)
+        }
+      });
+      console.log(tasks);
+      // this.setState({ task: tasks })
+      console.log(this.state.task);
+    }).catch(err => console.log(err))
+
+    handler.get("api/requests/requestinguser/" + this.props.user.id).then(res => {
+      // console.log(this.props.user.id);
+      // console.log(res.data);
+      res.data.forEach(element => {
+        if (element.isAccepet) {
+          tasks.push(element)
+        }
+      });
+    }).catch(err => console.log(err))
+
+    this.setState({ task: tasks })
+
   }
   render() {
-    return (
-      <div>
-        <h2>yooonisvenrocwin</h2>
-      </div>
-    )
+    return this.state.task.map(oneTask => {
+      return (
+        <div>
+          <h2>{oneTask.name}</h2>
+          <h1>You were accepted for this task</h1>
+        </div>
+      )
+    })
   }
 }
